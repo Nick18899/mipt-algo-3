283a38f5c7df7eb9ff780da81d34aab7b65330c1
Nick18899 <51851229+Nick18899@users.noreply.github.com>
2023-03-03 22:37:28 +0300

    Update E.cpp

--- E.cpp
+++ E.cpp
@@ -1,65 +1,89 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <stack>
+#include <vector>
 
-using namespace std;
+const long long kMaxVal = 100000;
 
-struct GasStation {
-  int start, end, x, id;
+struct Edge {
+  long long start;
+  long long end;
+  long long cost;
 };
 
+std::vector<Edge> TransformGraphToEdges(
+    std::vector<std::vector<long long>> graph, long long n) {
+  std::vector<Edge> edges;
+  for (long long i = 0; i < n; ++i) {
+    for (long long j = 0; j < n; ++j) {
+      if (graph[i][j] != kMaxVal) {
+        edges.push_back({i, j, graph[i][j]});
+      }
+    }
+  }
+  return edges;
+}
+
+void FordBellmanForCycles(std::vector<Edge>& edges, long long start,
+                          long long m, long long n) {
+  std::vector<long long> distances(n);
+  std::vector<long long> cycle(n, -1);
+  distances[start] = 0;
+  long long flag = -1;
+  for (long long i = 0; i < n; ++i) {
+    flag = -1;
+    for (long long j = 0; j < m; ++j) {
+      if (distances[edges[j].end] > distances[edges[j].start] + edges[j].cost) {
+        distances[edges[j].end] =
+            std::max(-kMaxVal, distances[edges[j].start] + edges[j].cost);
+        cycle[edges[j].end] = edges[j].start;
+        flag = edges[j].end;
+      }
+    }
+  }
+  if (flag == -1) {
+    std::cout << "NO";
+  } else {
+    long long buffer = flag;
+    for (long long i = 0; i < n; ++i) {
+      buffer = cycle[buffer];
+      if (buffer == -1) {
+        std::cout << "NO";
+        return;
+      }
+    }
+    std::vector<long long> path;
+    for (long long i = buffer;; i = cycle[i]) {
+      path.push_back(i);
+      if (i == buffer && path.size() > 1) {
+        break;
+      }
+    }
+    std::reverse(path.begin(), path.end());
+    std::cout << "YES" << '\n' << path.size() << '\n';
+    for (auto i : path) {
+      std::cout << i + 1 << " ";
+    }
+  }
+}
+
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  int n, x1, y1;
-  cin >> n >> y1 >> x1;
-  vector<GasStation> gus_stations(n);
-  for (int i = 0; i < n; ++i) {
-    gus_stations[i].id = i;
-    cin >> gus_stations[i].x >> gus_stations[i].start >> gus_stations[i].end;
-  }
-  multiset<int> locations = {0, x1};
-  multiset<int> distances = {x1};
-  vector<vector<pair<bool, int>>> events(y1 + 1);
-  for (const GasStation& a : gus_stations) {
-    events[a.start].push_back({true, a.id});
-    events[a.end].push_back({false, a.id});
-  }
-  for (int height = 0; height <= y1; ++height) {
-    vector<int> adding, removing;
-    for (int i = 0; i < (long long)events[height].size(); ++i) {
-      bool type = events[height][i].first;
-      int id = events[height][i].second;
-      if (type) {
-        int x = gus_stations[id].x;
-        int before = *std::prev(locations.lower_bound(x));
-        int after = *locations.lower_bound(x + 1);
-        distances.erase(distances.lower_bound(after - before));
-        distances.insert(x - before);
-        distances.insert(after - x);
-        locations.insert(x);
-      }
-    }
-    cout << *distances.rbegin() << '\n';
-    for (int& x : adding) {
-      locations.insert(x);
-    }
-    for (int i = 0; i < (long long)events[height].size(); ++i) {
-      bool type = events[height][i].first;
-      int id = events[height][i].second;
-      if (!type) {
-        int x = gus_stations[id].x;
-        int before = *std::prev(locations.lower_bound(x));
-        int after = *locations.lower_bound(x + 1);
-        distances.erase(distances.lower_bound(after - x));
-        distances.erase(distances.lower_bound(x - before));
-        distances.insert(after - before);
-        locations.erase(locations.lower_bound(x));
-      }
-    }
-    for (int x : removing) {
-      locations.erase(locations.lower_bound(x));
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  long long n, val;
+  std::cin >> n;
+  std::vector<std::vector<long long>> graph;
+  graph.assign(n, std::vector<long long>(n, kMaxVal));
+  for (long long i = 0; i < n; ++i) {
+    for (long long j = 0; j < n; ++j) {
+      std::cin >> val;
+      graph[i][j] = val;
     }
   }
-  cout.flush();
+  auto edges = TransformGraphToEdges(graph, n);
+  FordBellmanForCycles(edges, 0, static_cast<long long>(edges.size()), n);
   return 0;
 }
