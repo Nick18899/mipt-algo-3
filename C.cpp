b1e0017aa94bf352a67ecd73cc8e96c81518c36a
Nick18899 <51851229+Nick18899@users.noreply.github.com>
2023-03-20 21:22:28 +0300

    Update C.cpp

--- C.cpp
+++ C.cpp
@@ -1,226 +1,81 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <vector>
 
-#include <cmath>
-
-#include <string>
-
-class AVLTree {
+class DST {
  public:
-  struct Node {
-    Node* left{nullptr};
-    Node* right{nullptr};
-    std::pair<long long, std::string> value;
-    unsigned char height{0};
-  };
-
-  AVLTree() : root_(nullptr) {}
-
-  void Insert(long long value, std::string sec) {
-    root_ = Insert(root_, value, sec);
+  DST(long long n) : parent_(n), rank_(n, 0) {
+    for (long long i = 0; i < n; ++i) {
+      parent_[i] = i;
+    }
   }
 
-  ~AVLTree() { Clear(root_); }
+  bool IsSame(long long u, long long v) { return Find(u) == Find(v); }
 
-  long long Height() { return (long long)root_->height; }
-
-  void Clear() {
-    Clear(root_);
-    root_ = nullptr;
+  long long Find(long long v) {
+    if (parent_[v] == v) return v;
+    parent_[v] = Find(parent_[v]);
+    return parent_[v];
   }
 
-  std::string FindValueByKey(long long value) {
-    return FindValueByKey(root_, value);
+  void Union(long long u, long long v) {
+    long long root_u = Find(u);
+    long long root_v = Find(v);
+    if (root_u != root_v) {
+      if (rank_[root_u] < rank_[root_v]) {
+        std::swap(root_v, root_u);
+      }
+      parent_[root_v] = root_u;
+      if (rank_[root_u] == rank_[root_v]) {
+        rank_[root_u] += 1;
+      }
+    }
   }
-
-  bool BFindByValue(long long value) { return BFindByValue(root_, value); }
-
-  void Erase(long long value) { root_ = Erase(root_, value); }
 
  private:
-  static Node* Erase(Node* node, long long value) {
-    if (node == nullptr) {
-      return node;
-    }
-    if (node->value.first == value) {
-      Node* left = node->left;
-      Node* right = node->right;
-      delete node;
-      if (right == nullptr) {
-        return FixBalance(left);
-      }
-      Node* min = FindMin(right);
-      min->right = UnlinkMin(right);
-      min->left = left;
-      return FixBalance(min);
-    }
-    if (value > node->value.first) {
-      node->right = Erase(node->right, value);
-    } else {
-      node->left = Erase(node->left, value);
-    }
-    return FixBalance(node);
-  }
-
-  static Node* FindMin(Node* node) {
-    if (node->left == nullptr) {
-      return node;
-    }
-    return FindMin(node->left);
-  }
-
-  static Node* UnlinkMin(Node* node) {
-    if (node->left == nullptr) {
-      return node->right;
-    }
-    node->left = UnlinkMin(node->left);
-    return FixBalance(node);
-  }
-
-  static bool BFindByValue(Node* node, long long value) {
-    if (node == nullptr) {
-      std::cout << "false";
-      return false;
-    }
-    if (node->value.first == value) {
-      return true;
-    }
-    if (value < node->value.first) {
-      return BFindByValue(node->left, value);
-    }
-    return BFindByValue(node->right, value);
-  }
-
-  static std::string FindValueByKey(Node* node, long long value) {
-    if (node == nullptr) {
-      return "-1";
-    }
-    if (node->value.first == value) {
-      return node->value.second;
-    }
-    if (value < node->value.first) {
-      return FindValueByKey(node->left, value);
-    }
-    return FindValueByKey(node->right, value);
-  }
-
-  static void Clear(Node* node) {
-    if (node != nullptr) {
-      if (node->left != nullptr) {
-        Clear(node->left);
-      }
-      if (node->right != nullptr) {
-        Clear(node->right);
-      }
-      delete node;
-    }
-  }
-
-  static Node* Insert(Node* node, long long value, std::string sec) {
-    if (node == nullptr) {
-      Node* new_node = new Node;
-      new_node->value.first = value;
-      new_node->value.second = sec;
-      return new_node;
-    }
-    if (value <= node->value.first) {
-      node->left = Insert(node->left, value, sec);
-    } else {
-      node->right = Insert(node->right, value, sec);
-    }
-    return FixBalance(node);
-  }
-
-  static long long Height(Node* node) {
-    return node == nullptr ? 0 : static_cast<long long>(node->height);
-  }
-
-  static long long BalanceFactor(Node* node) {
-    return node == nullptr ? 0 : Height(node->right) - Height(node->left);
-  }
-
-  static void CalcHeight(Node* node) {
-    if (node == nullptr) {
-      return;
-    }
-    node->height = std::max(Height(node->left), Height(node->right)) + 1;
-  }
-
-  static Node* RightRotate(Node* p) {
-    Node* q = p->left;
-    p->left = q->right;
-    q->right = p;
-    CalcHeight(p);
-    CalcHeight(q);
-    return q;
-  }
-
-  static Node* LeftRotate(Node* q) {
-    Node* p = q->right;
-    q->right = p->left;
-    p->left = q;
-    CalcHeight(q);
-    CalcHeight(p);
-    return p;
-  }
-
-  static Node* FixBalance(Node* node) {
-    CalcHeight(node);
-    if (BalanceFactor(node) == 2) {
-      if (BalanceFactor(node->right) == -1) {
-        node->right = RightRotate(node->right);
-      }
-      return LeftRotate(node);
-    }
-    if (BalanceFactor(node) == -2) {
-      if (BalanceFactor(node->left) == 1) {
-        node->left = LeftRotate(node->left);
-      }
-      return RightRotate(node);
-    }
-    return node;
-  }
-
-  Node* root_;
+  std::vector<long long> parent_;
+  std::vector<long long> rank_;
 };
 
-long long StringToNumber(std::string& s, const long long* arr) {
-  long long hash_code = 0;
-  for (long long i = 0; i < (long long)s.size(); ++i) {
-    hash_code = (hash_code + (s[i] - 'a' + 1) * arr[i]) % (1000000000 + 7);
+struct Edge {
+  long long from;
+  long long to;
+  long long weight;
+};
+
+std::vector<Edge> MSTKruscal(long long n, std::vector<Edge>& edges) {
+  std::sort(edges.begin(), edges.end(), [](const Edge& e1, const Edge& e2) {
+    return e1.weight < e2.weight;
+  });
+  DST dst_vertexes(n);
+  std::vector<Edge> answer;
+  answer.reserve(n - 1);
+  long long founded = 0;
+  for (auto& edge : edges) {
+    if (!dst_vertexes.IsSame(edge.from, edge.to)) {
+      ++founded;
+      dst_vertexes.Union(edge.from, edge.to);
+      answer.push_back(edge);
+    }
+    if (founded == n - 1) {
+      break;
+    }
   }
-  return hash_code;
+  return answer;
 }
 
 int main() {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
-  long long* arr = new long long[1000000];
-  long long cnst = 29;
-  arr[0] = 1;
-  for (long long i = 1; i < 1000000; ++i) {
-    arr[i] = (arr[i - 1] * cnst) % (1000000000 + 7);
+  long long n, m;
+  std::cin >> n >> m;
+  std::vector<Edge> edges;
+  for (long long i = 0; i < m; ++i) {
+    long long from, to, weight;
+    std::cin >> from >> to >> weight;
+    edges.push_back({--from, --to, weight});
   }
-  AVLTree avl1;
-  AVLTree avl2;
-  long long n, q;
-  std::cin >> n;
-  std::string x, y;
-  for (int i = 0; i < n; ++i) {
-    std::cin >> x >> y;
-    avl1.Insert(StringToNumber(x, arr), y);
-    avl2.Insert(StringToNumber(y, arr), x);
-  }
-  std::cin >> q;
-  for (int i = 0; i < q; ++i) {
-    std::cin >> x;
-    long long ind = StringToNumber(x, arr);
-    std::string res = avl1.FindValueByKey(ind);
-    if (res != "-1") {
-      std::cout << res << std::endl;
-    } else {
-      std::cout << avl2.FindValueByKey(ind) << std::endl;
-    }
-  }
-  delete[] arr;
+  auto mst = MSTKruscal(n, edges);
+  std::cout << std::accumulate(
+      mst.begin(), mst.end(), 0ull,
+      [](long long acc, const Edge& edge) { return acc + edge.weight; });
 }
